adf16ae508ba65750bd359cffcb2a006bfbec505
YashasKirito <60006722+YashasKirito@users.noreply.github.com>
2020-08-17 14:38:51 +0530

    Update settings.py

--- SpamFilter/settings.py
+++ SpamFilter/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "x#fzg09^gu-ah5*u-+ix-x3b!8ack&s=a*sp-j*7o&r$ah8@t6"
+SECRET_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
